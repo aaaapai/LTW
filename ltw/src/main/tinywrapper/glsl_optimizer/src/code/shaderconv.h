6368f3bfd43a25f56a62240b516517c38e6dece5
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-02 13:10:57 +0800

    Create shaderconv.h

---
+++ ltw/src/main/tinywrapper/glsl_optimizer/src/code/shaderconv.h
@@ -0,0 +1,10 @@
+//
+// Created by hanji on 2024/10/20.
+//
+
+#ifndef FOLD_CRAFT_LAUNCHER_SHADERCONV_H
+#define FOLD_CRAFT_LAUNCHER_SHADERCONV_H
+
+char* MesaConvertShader(const char * const *src, unsigned int type, unsigned int glsl, unsigned int essl);
+
+#endif //FOLD_CRAFT_LAUNCHER_SHADERCONV_H
