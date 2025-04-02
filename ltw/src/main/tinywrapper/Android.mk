11bf6609760ba0d372c80092ee4b8f405376021c
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-02 13:00:14 +0800

    Update Android.mk

--- ltw/src/main/tinywrapper/Android.mk
+++ ltw/src/main/tinywrapper/Android.mk
@@ -372,39 +372,16 @@ LOCAL_SRC_FILES := glsl_optimizer/src/code/ir_print_glsl_visitor.cpp \
 LOCAL_CFLAGS += -D_LIB
 LOCAL_CFLAGS += -DNOMINMAX
 LOCAL_CFLAGS += -D_USE_MATH_DEFINES
-LOCAL_CFLAGS += -D__STDC_NO_THREADS__
 LOCAL_CFLAGS += -D__STDC_LIMIT_MACROS
 LOCAL_CFLAGS += -D__STDC_FORMAT_MACROS
 LOCAL_CFLAGS += -D__STDC_CONSTANT_MACROS
 LOCAL_CFLAGS += -DUTIL_ARCH_LITTLE_ENDIAN
-LOCAL_CFLAGS += -DUNIX
+LOCAL_CFLAGS += -DUNIX -DLINUX
 LOCAL_CFLAGS += -DANDROID -DHAVE_STRUCT_TIMESPEC -DDETECT_OS_ANDROID
 LOCAL_CFLAGS += -DHAVE_OPENGL
 LOCAL_CFLAGS += -DHAVE_OPENGL_ES_1
 LOCAL_CFLAGS += -DHAVE_OPENGL_ES_2
-LOCAL_CFLAGS += -fvisibility=hidden
+LOCAL_CFLAGS += -DHAVE_OPENGL_ES_3
+LOCAL_LDLIBS += -fuse-ld=lld -flto=thin -Wl,-plugin-opt=-emulated-tls=0
+LOCAL_CFLAGS += -fvisibility=hidden -O3 -pipe -integrated-as -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-invariant-load-hoisting -flto=thin -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -mllvm -polly-omp-backend=LLVM -mllvm -polly-scheduling=dynamic -flto=thin -fno-emulated-tls -fwhole-program-vtables -fdata-sections -ffunction-sections -fmerge-all-constants -mllvm -hot-cold-split=true -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -march=armv8-a+simd -fvisibility=hidden -Wall -Wextra -Wno-format -Wno-return-type -ferror-limit=0
 include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := ltw
-LOCAL_SRC_FILES := \
-    egl.c \
-    proc.c \
-    main.c \
-    glformats.c \
-    basevertex.c \
-    shader_wrapper.c \
-    string_utils.c \
-    framebuffer.c \
-    of_buffer_copier.c \
-    stubs.c \
-    multidraw.c \
-    vertexattrib.c \
-    swizzle.c \
-    license_notice.c \
-    vgpu_shaderconv/shaderconv.c \
-    unordered_map/unordered_map.c \
-    unordered_map/int_hash.c
-LOCAL_STATIC_LIBRARIES := glsl_optimizer
-LOCAL_LDLIBS := -llog -lEGL
-include $(BUILD_SHARED_LIBRARY)
