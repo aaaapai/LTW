f6df253731cc71c33693da7cc266a8edee20db23
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-02 23:06:11 +0800

    Update GlslConvert.cpp

--- ltw/src/main/tinywrapper/glsl_optimizer/src/code/GlslConvert.cpp
+++ ltw/src/main/tinywrapper/glsl_optimizer/src/code/GlslConvert.cpp
@@ -60,7 +60,7 @@ GlslConvert::~GlslConvert()
 ///////////////////////////////////////////////////////////////////////////////
 
 char * GlslConvert::Optimize(
-	const char *const * vShaderSource,
+	const char *vShaderSource,
 	ShaderStage vShaderType,
 	ApiTarget vTarget,
 	LanguageTarget vLanguageTarget,
@@ -123,7 +123,7 @@ char * GlslConvert::Optimize(
 	//_mesa_glsl_read_ir(state, shader->ir, input.c_str(), true);
 
 	shader->Source = vShaderSource;
-	const char *const * source = shader->Source;
+	const char *source = shader->Source;
 
 	if (!(vOptimizationStruct.controlFlags & ControlFlags::CONTROL_SKIP_PREPROCESSING))
 	{
