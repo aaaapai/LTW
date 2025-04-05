77959d6c6a9f111a3a8057509719c88b07492a2e
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-05 13:04:02 +0800

    Update Android.mk

--- ltw/src/main/tinywrapper/Android.mk
+++ ltw/src/main/tinywrapper/Android.mk
@@ -371,6 +371,7 @@ LOCAL_SRC_FILES := glsl_optimizer/src/code/ir_print_glsl_visitor.cpp \
    glsl_optimizer/src/compiler/nir/nir_lower_locals_to_regs.c
 LOCAL_CFLAGS += -D_LIB
 LOCAL_CFLAGS += -DNOMINMAX
+LOCAL_CFLAGS += -D__STDC_NO_THREADS__
 LOCAL_CFLAGS += -D_USE_MATH_DEFINES
 LOCAL_CFLAGS += -D__STDC_LIMIT_MACROS
 LOCAL_CFLAGS += -D__STDC_FORMAT_MACROS
@@ -382,7 +383,7 @@ LOCAL_CFLAGS += -DHAVE_OPENGL
 LOCAL_CFLAGS += -DHAVE_OPENGL_ES_1
 LOCAL_CFLAGS += -DHAVE_OPENGL_ES_2
 LOCAL_CFLAGS += -DHAVE_OPENGL_ES_3
-LOCAL_LDLIBS += -fuse-ld=lld -flto=thin -Wl,-plugin-opt=-emulated-tls=0 -llog
+LOCAL_LDLIBS += -llog
+# LOCAL_LDLIBS += -fuse-ld=lld -flto=thin -Wl,-plugin-opt=-emulated-tls=0 -llog
 # LOCAL_CFLAGS += -fvisibility=hidden -O3 -pipe -integrated-as -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-invariant-load-hoisting -flto=thin -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -mllvm -polly-omp-backend=LLVM -mllvm -polly-scheduling=dynamic -flto=thin -fno-emulated-tls -fwhole-program-vtables -fdata-sections -ffunction-sections -fmerge-all-constants -mllvm -hot-cold-split=true -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -march=armv8-a+simd -fvisibility=hidden -Wall -Wextra -Wno-format -Wno-return-type -ferror-limit=0
-LOCAL_CFLAGS += -pipe -flto=thin -fno-emulated-tls
 include $(BUILD_SHARED_LIBRARY)
