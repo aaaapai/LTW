4421c32ae5e1bf949147f9e4f69862ebbd592022
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-05 13:07:07 +0800

    Update Android.mk

--- ltw/src/main/tinywrapper/Android.mk
+++ ltw/src/main/tinywrapper/Android.mk
@@ -369,7 +369,8 @@ LOCAL_SRC_FILES := glsl_optimizer/src/code/ir_print_glsl_visitor.cpp \
    glsl_optimizer/src/compiler/nir/nir_lower_memcpy.c \
    glsl_optimizer/src/compiler/nir/nir_intrinsics.c \
    glsl_optimizer/src/compiler/nir/nir_lower_locals_to_regs.c
-LOCAL_CFLAGS += -D_LIB
+LOCAL_CPPLAGS += -std=c++20
+LOCAL_CFLAGS += -D_LIB -std=c11
 LOCAL_CFLAGS += -DNOMINMAX
 LOCAL_CFLAGS += -D__STDC_NO_THREADS__
 LOCAL_CFLAGS += -D_USE_MATH_DEFINES
