bae9360a2202b943891a456712b7489c04f134bf
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-06-07 18:11:27 +0800

    Update shaderconv.c

--- ltw/src/main/tinywrapper/glsl_optimizer/src/code/shaderconv.c
+++ ltw/src/main/tinywrapper/glsl_optimizer/src/code/shaderconv.c
@@ -6,6 +6,8 @@
 
 #include "c_wrapper.h"
 
+char* (*shaderconv_optimize_shader)(const char * const *src, GLenum type, unsigned int glsl, unsigned int essl);
+
 __attribute((visibility("default"))) char* MesaConvertShader(const char *src, GLenum type, unsigned int glsl, unsigned int essl) {
-    return optimize_shader(src, type, glsl, essl);
+    return shaderconv_optimize_shader(src, type, glsl, essl);
 }
