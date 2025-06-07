42a2ea4d6c5a66d86ff1406f65ef6f9b1e4267a4
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-06-07 17:39:09 +0800

    Update Android.mk

--- ltw/src/main/tinywrapper/Android.mk
+++ ltw/src/main/tinywrapper/Android.mk
@@ -369,6 +369,7 @@ LOCAL_SRC_FILES := glsl_optimizer/src/code/ir_print_glsl_visitor.cpp \
    glsl_optimizer/src/compiler/nir/nir_lower_memcpy.c \
    glsl_optimizer/src/compiler/nir/nir_intrinsics.c \
    glsl_optimizer/src/compiler/nir/nir_lower_locals_to_regs.c
+LOCAL_CPPLAGS += -std=c23
 LOCAL_CPPLAGS += -std=c++20
 LOCAL_CFLAGS += -D_LIB
 LOCAL_CFLAGS += -DNOMINMAX
@@ -384,8 +385,23 @@ LOCAL_CFLAGS += -DHAVE_OPENGL
 LOCAL_CFLAGS += -DHAVE_OPENGL_ES_1
 LOCAL_CFLAGS += -DHAVE_OPENGL_ES_2
 LOCAL_CFLAGS += -DHAVE_OPENGL_ES_3
+LOCAL_CPPFLAGS += -D_LIB
+LOCAL_CPPFLAGS += -DNOMINMAX
+LOCAL_CPPFLAGS += -D__STDC_NO_THREADS__
+LOCAL_CPPFLAGS += -D_USE_MATH_DEFINES
+LOCAL_CPPFLAGS += -D__STDC_LIMIT_MACROS
+LOCAL_CPPFLAGS += -D__STDC_FORMAT_MACROS
+LOCAL_CPPFLAGS += -D__STDC_CONSTANT_MACROS
+LOCAL_CPPFLAGS += -DUTIL_ARCH_LITTLE_ENDIAN
+LOCAL_CPPFLAGS += -DUNIX -DLINUX
+LOCAL_CPPFLAGS += -DANDROID -DHAVE_STRUCT_TIMESPEC -DDETECT_OS_ANDROID
+LOCAL_CPPFLAGS += -DHAVE_OPENGL
+LOCAL_CPPFLAGS += -DHAVE_OPENGL_ES_1
+LOCAL_CPPFLAGS += -DHAVE_OPENGL_ES_2
+LOCAL_CPPFLAGS += -DHAVE_OPENGL_ES_3
 LOCAL_CPPLAGS += -stdlib=libc++
-LOCAL_LDLIBS += -llog
-# LOCAL_LDLIBS += -fuse-ld=lld -flto=thin -Wl,-plugin-opt=-emulated-tls=0 -llog
-# LOCAL_CFLAGS += -fvisibility=hidden -O3 -pipe -integrated-as -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-invariant-load-hoisting -flto=thin -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -mllvm -polly-omp-backend=LLVM -mllvm -polly-scheduling=dynamic -flto=thin -fno-emulated-tls -fwhole-program-vtables -fdata-sections -ffunction-sections -fmerge-all-constants -mllvm -hot-cold-split=true -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -march=armv8-a+simd -fvisibility=hidden -Wall -Wextra -Wno-format -Wno-return-type -ferror-limit=0
+LOCAL_LDLIBS += -llog -lc++_static -lc++abi
+LOCAL_LDLIBS += -fuse-ld=lld -flto=thin -Wl,-plugin-opt=-emulated-tls=0 -llog
+LOCAL_CFLAGS += -fvisibility=hidden -O3 -pipe -integrated-as -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-invariant-load-hoisting -flto=thin -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -mllvm -polly-omp-backend=LLVM -mllvm -polly-scheduling=dynamic -flto=thin -fno-emulated-tls -fwhole-program-vtables -fdata-sections -ffunction-sections -fmerge-all-constants -mllvm -hot-cold-split=true -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -march=armv8-a+simd -fvisibility=hidden -Wall -Wextra -Wno-format -Wno-return-type -ferror-limit=0
+LOCAL_CPPFLAGS += -fvisibility=hidden -O3 -pipe -integrated-as -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-invariant-load-hoisting -flto=thin -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -mllvm -polly-omp-backend=LLVM -mllvm -polly-scheduling=dynamic -flto=thin -fno-emulated-tls -fwhole-program-vtables -fdata-sections -ffunction-sections -fmerge-all-constants -mllvm -hot-cold-split=true -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -march=armv8-a+simd -fvisibility=hidden -Wall -Wextra -Wno-format -Wno-return-type -ferror-limit=0
 include $(BUILD_SHARED_LIBRARY)
