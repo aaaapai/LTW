7a468c4ca241c9cc6d6e4b1fc0f3a0565b87c104
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-31 13:00:52 +0800

    Update init.h

--- ltw/src/main/tinywrapper/glsl_optimizer/src/code/init.h
+++ ltw/src/main/tinywrapper/glsl_optimizer/src/code/init.h
@@ -5,6 +5,16 @@
 #ifndef FOLD_CRAFT_LAUNCHER_INIT_H
 #define FOLD_CRAFT_LAUNCHER_INIT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void MesaConverterInit();
+void *open_lib(const char **names, const char *override);
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
 
 #endif //FOLD_CRAFT_LAUNCHER_INIT_H
