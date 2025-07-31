d282b20685a8c185c3fea6eb0cac2662ce2fd43f
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-31 12:52:00 +0800

    Update Android.mk

--- ltw/src/main/tinywrapper/Android.mk
+++ ltw/src/main/tinywrapper/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := shaderconv
 LOCAL_SRC_FILES := glsl_optimizer/src/code/ir_print_glsl_visitor.cpp \
    glsl_optimizer/src/code/optimizer.cpp \
+   glsl_optimizer/src/code/c_wrapper.cpp \
    glsl_optimizer/src/code/GlslConvert.cpp \
    glsl_optimizer/src/code/ir_print_ir_visitor.cpp \
    glsl_optimizer/src/util/compat_layer.cpp \
@@ -368,8 +369,8 @@ LOCAL_SRC_FILES := glsl_optimizer/src/code/ir_print_glsl_visitor.cpp \
    glsl_optimizer/src/compiler/nir/nir_lower_memcpy.c \
    glsl_optimizer/src/compiler/nir/nir_intrinsics.c \
    glsl_optimizer/src/compiler/nir/nir_lower_locals_to_regs.c
-LOCAL_CPPLAGS += -std=c++20 -D_LIB -DNOMINMAX -D__STDC_NO_THREADS__ -D_USE_MATH_DEFINES -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_CONSTANT_MACROS -DUTIL_ARCH_LITTLE_ENDIAN -DUNIX -DLINUX -DANDROID -DHAVE_STRUCT_TIMESPEC -DDETECT_OS_ANDROID -DHAVE_OPENGL -DHAVE_OPENGL_ES_1 -DHAVE_OPENGL_ES_2 -DHAVE_OPENGL_ES_3
-LOCAL_CONLYFLAGS += -D_LIB -std=c11
+LOCAL_CPPLAGS += -std=c++23 -D_LIB -DNOMINMAX -D__STDC_NO_THREADS__ -D_USE_MATH_DEFINES -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_CONSTANT_MACROS -DUTIL_ARCH_LITTLE_ENDIAN -DUNIX -DLINUX -DANDROID -DHAVE_STRUCT_TIMESPEC -DDETECT_OS_ANDROID -DHAVE_OPENGL -DHAVE_OPENGL_ES_1 -DHAVE_OPENGL_ES_2 -DHAVE_OPENGL_ES_3
+LOCAL_CONLYFLAGS += -D_LIB -std=c23
 LOCAL_CFLAGS += -DNOMINMAX
 LOCAL_CFLAGS += -D__STDC_NO_THREADS__
 LOCAL_CFLAGS += -D_USE_MATH_DEFINES
@@ -386,5 +387,5 @@ LOCAL_CFLAGS += -DHAVE_OPENGL_ES_3
 LOCAL_CPPLAGS += -stdlib=libc++
 LOCAL_LDLIBS += -llog
 LOCAL_LDLIBS += -fuse-ld=lld -llog -lc++_static -lc++abi
-LOCAL_CFLAGS += -O1 -mllvm -polly -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-parallel -march=armv8-a+simd -Wall -Wextra -Wno-format -Wno-return-type -ferror-limit=0
+LOCAL_CFLAGS += -O1 -Wall -Wextra -Wno-format -Wno-return-type -ferror-limit=0
 include $(BUILD_STATIC_LIBRARY)
