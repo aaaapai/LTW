f5093b797b1be47828fe381ce1ae5a88a16619e8
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-15 18:13:22 +0800

    Update shaderconv.h

--- ltw/src/main/tinywrapper/glsl_optimizer/src/code/shaderconv.h
+++ ltw/src/main/tinywrapper/glsl_optimizer/src/code/shaderconv.h
@@ -11,7 +11,7 @@
 extern "C" {
 #endif
 
-char* MesaConvertShader(const char *src, GLenum type, unsigned int glsl, unsigned int essl);
+std:: MesaConvertShader(const char *src, GLenum type, unsigned int glsl, unsigned int essl);
 
 #ifdef __cplusplus
 } /* extern C */
